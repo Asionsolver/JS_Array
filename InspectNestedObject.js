127b6074149ce34d6fe34997e04ba034afd68a67
Asionsolver <asionsolver274@gmail.com>
2023-04-29 23:24:00 +0600

    add Inspect Nested Object .js file

---
+++ InspectNestedObject.js
@@ -0,0 +1,76 @@
+/**
+* @title Inspect Nested Object
+*/
+
+const js = {
+    name: 'JavaScript: All You Need to Know',
+    author : {
+        name: 'HM Nayem',
+        email: 'jsan.m.nayem@gamail.com',
+        profession: ' Full Stack Developer',
+        experience: '10 Years'
+    },
+    contents: {
+        video: {
+            count: 200,
+        },
+    },
+};
+
+const node = {
+    name: 'Node.js: All You Need to Know',
+    author : {
+        name: 'HM Nayem',
+        email: 'hsan.nayem@gmail.con',
+        profession: ' Full Stack Developer',
+        experience: '10 Years'
+    },
+    contents: {
+        video: {
+            count: 174,
+        },
+    },
+};
+
+const react = {
+    name: 'React.js: All You Need to Know',
+    author : {
+        name: 'HM Nayem',
+        email: 'hsan.nayem@gmail.con',
+        profession: ' Full Stack Developer',
+        experience: '10 Years'
+    },
+    contents: {
+        video: {
+            count: 98,
+        },
+        article: {
+            count: 20,
+        },
+        quiz: {
+            count: 10,
+        },
+    },
+};
+
+
+
+
+const inspectObject = (obj, path) => {
+    return path.split('.').reduce((acc, curr) => {
+        if (acc[curr]) {
+            return acc[curr];
+        }
+        return 0;
+    }, obj);
+};
+
+const course = [js, node, react];
+
+course.forEach((course) => {
+    const videoCount = inspectObject(course, 'contents.video.count');
+    const articleCount = inspectObject(course, 'contents.article.count');
+    const quizCount = inspectObject(course, 'contents.quiz.count');
+    console.log(`${course.name} has ${videoCount} videos, ${articleCount} articles and ${quizCount} quizzes`);
+});
+
